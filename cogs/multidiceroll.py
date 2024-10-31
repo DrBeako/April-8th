b00e391e4a76782a903fb043d518900802ea12eb
Lavinia <100luxxxxxxxxxxxx@gmail.com>
2024-10-31 11:54:53 -0700

    dice now fits in 1 message

--- cogs/multidiceroll.py
+++ cogs/multidiceroll.py
@@ -17,12 +17,16 @@ class MultiDiceRoll(commands.Cog):
     async def errorCode(self, ctx: discord, amount: int) -> None:
         # do the stuff
         rolltime = 0
-        total = []
+        total = "none"
         while rolltime < amount:
             result = random.randint(1,8)
             word = convert_id(result)
-            await ctx.respond(f"{word}")
             rolltime = rolltime + 1
+            if total == "none":
+                total = word
+            total = word + total
+
+        await ctx.respond(f"{total}")
 
 
     @errorCode.error  # type: ignore
